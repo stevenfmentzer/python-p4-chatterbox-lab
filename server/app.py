0fa23197059a984a2e92c0d1b2cf9a50f364ef53
Steven Mentzer <stevenfmentzer@gmail.com>
2024-02-05 22:41:13 -0500

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,86 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+        if request.method == 'GET':
+            try:
+                messages = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+                response = make_response(
+                    messages,
+                    200 
+                )
+            except: 
+                response = make_response(
+                {"ERROR" : "COULDN'T FIND MESSAGES"},
+                404
+                )
+            return response
+        elif request.method == 'POST':
+            try:
+                if request.headers.get("Content-Type") == 'application/json':
+                    form_data = request.get_json()
+                else: 
+                    form_data = request.form
+                new_message = Message(
+                    body = form_data['body'],
+                    username = form_data['username']
+                )
+                db.session.add(new_message)
+                db.session.commit()
 
-@app.route('/messages/<int:id>')
+                response = make_response(
+                    new_message.to_dict(), 
+                    201
+                )
+            except Exception as e:
+                response = make_response(
+                    {"ERROR" : str(e)}, 
+                    500
+                )
+            return response
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    try:
+        message = Message.query.filter(Message.id == id).first()
+        if request.method == 'PATCH':
+            try:
+                if request.headers.get("Content-Type") == 'application/json':
+                    form_data = request.get_json()
+                else: 
+                    form_data = request.form
+
+                for attr in form_data:
+                    setattr(message, attr, form_data[attr])
+                db.session.commit()
+
+                response = make_response(
+                    message.to_dict(), 
+                    200
+                )
+
+            except Exception as e:
+                response = make_response(
+                    {"ERROR" : str(e)}, 
+                    500
+                )
+            return response
+        
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response = make_response(
+                {"SUCCESS" : "Message Deleted"},
+                202
+            )
+    except: 
+        response = make_response(
+            {"ERROR" : "COULDN'T FIND MESSAGE"},
+            404
+        )
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
